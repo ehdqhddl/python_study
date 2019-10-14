b415cff9330db4f87f28d9c4b3670496b2af149c
ehdqhddl <ehdqhddl91@gmail.com>
2019-10-14 20:56:21 +0900

    파이썬 몸풀기 코딩

---
+++ section02-2.py
@@ -0,0 +1,24 @@
+# Section02-2 
+# 파이썬 기초 코딩
+# 몸풀기 코딩 실습
+
+#import this
+import sys
+
+# 파이썬 기본 인코딩
+print(sys.stdin.encoding)
+print(sys.stdout.encoding)
+
+# 출력문
+print('My name is ')
+
+# 변수 선언
+myName = 'GoodBoy'
+print(myName)
+
+# 조건문
+
+if myName == "GoodBoy":
+    print("OK")
+else:
+    print("NO")
