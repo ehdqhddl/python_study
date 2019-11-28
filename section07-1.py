ae1fba41a9fdb97a9702c6684b5a9738b3f1a08c
ehdqhddl <ehdqhddl91@gmail.com>
2019-11-29 00:40:47 +0900

    클래스 기본 이해

--- section07-1.py
+++ section07-1.py
@@ -2,7 +2,7 @@
 # 파이썬 클래스 상세 이해
 # self,  클래스, 인스턴스 변수
 
-
+# 예제1
 # 선언
 
 class UserInfo:
@@ -26,3 +26,65 @@ print(id(user2))
 # 네임스페이스 출력
 print(user1.__dict__)
 print(user2.__dict__)
+
+
+# 예제2
+# self의 이해
+
+class SelfTest:
+    @staticmethod
+    def func1():
+        print("func1 called!")
+    def func2(self):
+        print("func2 called!")
+
+SelfTest.func1()
+self_test = SelfTest()
+self_test.func2()
+
+
+# 예제3
+# 클래스 변수, 인스턴스 변수
+# 클래스 변수 = java의 static 변수
+
+class WareHouse:
+    # 클래스 변수
+    stock_num = 0
+    def __init__(self,name):
+        self.name = name
+        WareHouse.stock_num += 1
+    def __del__(self):
+        WareHouse.stock_num -= 1
+
+user1 = WareHouse('Park')
+user2 = WareHouse('Kim')
+user3 = WareHouse('Lee')
+
+print(user1.__dict__)
+print(user2.__dict__)
+print(user3.__dict__)
+print(WareHouse.__dict__) # 클래스 네임스페이스
+
+# 예제4
+# 다중 상속
+
+class X():
+    pass
+
+class Y():
+    pass
+
+class Z():
+    pass
+
+class A(X,Y):
+    pass
+
+class B(Y,Z):
+    pass
+
+class M(B,A,Z):
+    pass
+
+
+print(M.mro())
